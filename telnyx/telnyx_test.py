84a8edbcaba20b5fd8097199137cd70abb0bf5f1
Cyrus Sethna <csethna@gmail.com>
2017-07-31 14:34:17 -0500

    symbol-to-value table
    Added symbol-to-value table for integers 0-9. Added comments for
    documentation.

--- telnyx/telnyx_test.py
+++ telnyx/telnyx_test.py
@@ -1,9 +1,27 @@
 integers = list(range(1000))
+# Creates a list with integers 0 - 999
 
 palindromes = []
+# Creates blank list for storing base(10) palindromic integers
 
 def isPalindrome(*integers):
     for integer in integers:
         if str(integer) == str(integer)[::-1]:
             palindromes.append(integer)
-            print(palindromes)
+            return(palindromes)
+# User defined function loops through 'integers' list to detect palindromes
+# Palindromes are added to the new list 'palindromes'
+
+sym2va = {
+'0': 0,
+'1': 1,
+'2': 2,
+'3': 3,
+'4': 4,
+'5': 5,
+'6': 6,
+'7': 7,
+'8': 8,
+'9': 9,
+}
+# Create symbol-to-value table
